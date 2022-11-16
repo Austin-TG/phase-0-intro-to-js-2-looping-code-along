c5a763221f046dd6d6ca14a8fe028215fff63dd8
Austin Griffin <austintg94@gmail.com>
2022-11-15 19:07:30 -0700

    commit tree 16645f0378dfbfb65fbebc5859756cd56bc82813

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++)
+{
+    console.log(`I'm ${age} years old.  Happy birthday to me!`);
+}
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`wrapped ${gifts[i]} and added a bow!`);
+    }    
+    return gifts;
+}
+wrapGifts(gifts);
+
+const giftAnnounce = [];
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function writeCards(sNames, eName){
+    for (let i = 0; i < sNames.length; i++)
+    {
+        //console.log(`Thank you, ${sNames[i]}, for the wonderful ${eName} gift!`);
+        giftAnnounce[i] = `Thank you, ${sNames[i]}, for the wonderful ${eName} gift!`;
+    }
+    return giftAnnounce;
+}
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
+
+countDown(10);
+countDown(4);
